f9b5107472b29be72fe43cbb8efc8c9b14397c8e
Kevin Damm <github@kevindamm.com>
2025-02-18 23:01:43 -0500

    python solution to problem 18 using a dynamic programming approach

---
+++ python/p0018.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2024 Kevin Damm
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Problem 0018 - Maximum Path Sum I"""
+
+from typing import List
+
+def read_triangle(filepath: str) -> List[List[int]]:
+  numbers: List[List[int]] = []
+  with open(filepath) as f:
+    for line in f.readlines():
+      if line == "": continue
+      numbers.append([int(x) for x in line.split(" ") if len(x)])
+
+  return numbers
+
+def max_route(triangle: List[List[int]]) -> int:
+  maxes = []
+  for row in triangle:
+    next = []
+    if len(row) == 1:
+      maxes = row
+      continue
+
+    next.append(maxes[0]+row[0])
+    for i in range(1, len(row)-1):
+      next.append(max(maxes[i-1], maxes[i]) + row[i])
+    next.append(maxes[-1]+row[-1])
+    maxes = next
+
+  return max(maxes)
+
+
+if __name__ == "__main__":
+  triangle = read_triangle("./data/0018_triangle.txt")
+  answer = max_route(triangle)
+  print(answer)
+
