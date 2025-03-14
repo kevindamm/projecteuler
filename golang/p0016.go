ed2e27aa06ad122715166acb48b62a7f6cbe0bcf
Kevin Damm <github@kevindamm.com>
2025-03-13 22:14:58 -0400

    golang solution for problem 16

---
+++ golang/p0016.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2024 Kevin Damm
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// github:kevindamm/projecteuler/golang/p0016.go
+
+package solutions
+
+import "math/big"
+
+func SumOfDigitsOfPowersOfTwo(power int) int64 {
+	bignum, bigpow := big.NewInt(2), big.NewInt(int64(power))
+	digits := new(big.Int).Exp(bignum, bigpow, nil).Text(10)
+	digitToInt := func(digit rune) int { return int(digit - '0') }
+
+	sum := 0
+	for _, digit := range digits {
+		sum += digitToInt(digit)
+	}
+	return int64(sum)
+}
