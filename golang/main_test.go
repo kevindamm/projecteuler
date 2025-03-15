e0232426251b308b7e5cc2e2c8f457b66273a175
Kevin Damm <github@kevindamm.com>
2025-03-15 09:44:26 -0700

    refactor golang test harness to allow calling with an effectively variadic function signature
    these tests were required to conform to (int) -> int64 in order to facilitate a table-driven test format but not all problems can easily fit this type
    
    slightly less readable now but much more versatile, and still pretty readable (moreso than splitting them into a hundred test files, imho)

--- golang/main_test.go
+++ golang/main_test.go
@@ -29,40 +29,94 @@ import (
 )
 
 func TestAllProblems(t *testing.T) {
+	// A compromise between boilerplate and table-driven test structure.
+	// Up to a point (around problem 18, 19) these could all have had
+	// the same function signatures but then some problems have varying params
+	// (e.g. a Date input, or zero parameters for max triangle paths)
+	// and though they could be forced into an integer, that dissatisfies.
+	// We need the function calling to happen within the [testing.T.Run] context,
+	// so the functions are called within an inlined function definition.
 	tests := []struct {
 		name     string
-		test     func(int) int64
-		input    int
+		process  func() int64
 		expected int64
 	}{
-		{"p0001", solutions.SumOf3or5, 10, 23},
-		{"p0002", solutions.SumEvenFibonacciUntil, 100, 44},
-		{"p0003", solutions.LargestPrimeFactor, 13195, 29},
-		{"p0004", solutions.FindLargestPalindromeProduct, 100, 9009},
-		{"p0005", solutions.SmallestCommonMultiple, 10, 2520},
-		{"p0006", solutions.SqSumDifference, 10, 2640},
-		{"p0007", solutions.NthPrime, 6, 13},
-		{"p0008", solutions.DigitSeriesFile("../data/0008_digits.txt").
-			LargestAdjacentProduct, 4, 5832},
-		{"p0009", solutions.SpecialPythagoreanTriplet, 12, 60},
-		{"p0010", solutions.SummationOfPrimesBelow, 10, 17},
-		{"p0011", solutions.NumberGridFile("../data/0011_grid.txt").
-			GreatestProductOfAdjacent, 2, 9306},
-		{"p0012", solutions.FirstTriangleExceedingFactorCount, 5, 28},
-		{"p0013", solutions.NumberListFile("../data/0013_numbers.txt").
-			LargeSumLeadingDigits, 1, 5},
-		{"p0014", solutions.LongestCollatzChainStartingBelow, 100, 97},
-		{"p0015", solutions.CountSquareLatticePaths, 2, 6},
-		{"p0016", solutions.SumOfDigitsOfPowersOfTwo, 15, 26},
-		{"p0017-a", solutions.NumberLetterCount, 342, 23},
-		{"p0017-b", solutions.NumberLetterCount, 115, 20},
-		{"p0018", solutions.NumberTriangleFile("../data/0018_triangle.txt").
-			MaximumPathSum, 5, 390},
-		{"p0020", solutions.FactorialDigitSum, 10, 27},
+		{"p0001",
+			func() int64 { return solutions.SumOf3or5(10) },
+			23},
+		{"p0002",
+			func() int64 { return solutions.SumEvenFibonacciUntil(100) },
+			44},
+		{"p0003",
+			func() int64 { return solutions.LargestPrimeFactor(13195) },
+			29},
+		{"p0004",
+			func() int64 { return solutions.FindLargestPalindromeProduct(100) },
+			9009},
+		{"p0005",
+			func() int64 { return solutions.SmallestCommonMultiple(10) },
+			2520},
+		{"p0006",
+			func() int64 { return solutions.SqSumDifference(10) },
+			2640},
+		{"p0007",
+			func() int64 { return solutions.NthPrime(6) },
+			13},
+		{"p0008",
+			func() int64 {
+				return solutions.DigitSeriesFile("../data/0008_digits.txt").
+					LargestAdjacentProduct(4)
+			},
+			5832},
+		{"p0009",
+			func() int64 { return solutions.SpecialPythagoreanTriplet(12) },
+			60},
+		{"p0010",
+			func() int64 { return solutions.SummationOfPrimesBelow(10) },
+			17},
+		{"p0011",
+			func() int64 {
+				return solutions.NumberGridFile("../data/0011_grid.txt").
+					GreatestProductOfAdjacent(2)
+			},
+			9306},
+		{"p0012",
+			func() int64 { return solutions.FirstTriangleExceedingFactorCount(5) },
+			28},
+		{"p0013",
+			func() int64 {
+				return solutions.NumberListFile("../data/0013_numbers.txt").
+					LargeSumLeadingDigits(1)
+			},
+			5},
+		{"p0014",
+			func() int64 { return solutions.LongestCollatzChainStartingBelow(100) },
+			97},
+		{"p0015",
+			func() int64 { return solutions.CountSquareLatticePaths(2) },
+			6},
+		{"p0016",
+			func() int64 { return solutions.SumOfDigitsOfPowersOfTwo(15) },
+			26},
+		{"p0017-a",
+			func() int64 { return solutions.NumberLetterCount(342) },
+			23},
+		{"p0017-b",
+			func() int64 { return solutions.NumberLetterCount(115) },
+			20},
+		{"p0018",
+			func() int64 {
+				return solutions.NumberTriangleFile("../data/0018_triangle.txt").
+					MaximumPathSum(5)
+			},
+			390},
+		{"p0020",
+			func() int64 { return solutions.FactorialDigitSum(10) },
+			27},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result := tt.test(tt.input)
+			result := tt.process()
 			if result != tt.expected {
 				t.Errorf("test %s expected %d, got %d", tt.name, tt.expected, result)
 			}
