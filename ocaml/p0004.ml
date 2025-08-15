7a08528ae5d4f6a35b4585e6df2dd8c405cf034c
Kevin Damm <github@kevindamm.com>
2025-08-15 04:52:21 -0400

    solution to problem 4 in ocaml

---
+++ ocaml/p0004.ml
@@ -0,0 +1,79 @@
+(*
+Copyright (c) 2025 Kevin Damm
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+github:kevindamm/projecteuler/ocaml/p0004.ml
+*)
+
+(* Problem 0004 - Largest Palindrome Product *)
+
+
+
+(*
+let all_pairs from until =
+  Seq.int from
+    |> Seq.take_while (fun i -> i < until)
+
+
+let largest_paliprod pairs =
+  let take_last _ x = x in
+  let pairprod pair = ((fst pair) * (snd pair))
+  Seq.fold_left take_last (from-1)
+    (pairs |> Seq.map pairprod |> Seq.filter is_palindrome)
+
+*)
+
+let rec seq_const value = fun () ->
+  Seq.Cons (value,
+            seq_const value)
+
+let rec seq_inc from until = fun () ->
+  if from >= until
+  then Seq.Nil
+  else Seq.Cons (from,
+                 seq_inc (from + 1) until );;
+
+let all_pairs_desc from until =
+ let rec all_pairs_aux a b = fun () ->
+  if b >= from
+  then Seq.Cons ((a, b), all_pairs_aux a (b-1))
+  else begin
+    if a > from
+    then Seq.Cons ((a-1, a-1), all_pairs_aux (a-1) (a-2))
+    else Seq.Nil
+  end
+  in all_pairs_aux (until-1) (until-1)
+
+let prod_of_pair (a, b) = a * b
+
+let string_of_pair = function
+  | (a, b) -> (string_of_int a) ^ ", " ^ (string_of_int b)
+
+
+let _ = 
+  let take_largest acc x =
+    if x > acc then x else acc in
+  (all_pairs_desc 100 1000)
+  |> Seq.map prod_of_pair
+  |> Seq.filter is_palindrome
+  |> Seq.fold_left take_largest 0
+  |> Seq.map string_of_int
+  |> Seq.iter print_endline
